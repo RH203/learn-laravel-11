c42e058b17beba976508beebadc480001c743c9b
Raihan Firdaus <r1aihan2003f1irdaus@gmail.com>
2024-05-12 18:39:43 +0700

    init: first initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>CRUD - Laravel 11</title>
+  @vite('resources/css/app.css')
+</head>
+
+<body>
+  <div class="container mx-auto my-3.5 bg-slate-200 rounded-md">
+    
+  </div>
+  <script src="./node_modules/preline/dist/preline.js"></script>
+</body>
+
+</html>
